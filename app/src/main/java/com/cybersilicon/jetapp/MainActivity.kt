e2ecd5a73010b1ae72f6f37f06e0214ac27db127
Hocine-ic <hocinehoho53@gmail.com>
2024-10-18 12:20:13 +0200

    BttomBarAdded

--- app/src/main/java/com/cybersilicon/jetapp/MainActivity.kt
+++ app/src/main/java/com/cybersilicon/jetapp/MainActivity.kt
@@ -6,12 +6,46 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.BubbleChart
+import androidx.compose.material.icons.filled.ChatBubble
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Message
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.BubbleChart
+import androidx.compose.material.icons.outlined.ChatBubble
+import androidx.compose.material.icons.outlined.ChatBubbleOutline
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Message
+import androidx.compose.material.icons.rounded.ChatBubble
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.cybersilicon.jetapp.ui.theme.JetAppTheme
+
+//data class BottomNavigationItems(
+//    val title : String,
+//    val selectedIcon : ImageVector,
+//    val unselectedIcon : ImageVector,
+//    val hasNews : Boolean,
+//    val badgeCount: Int? = null
+//)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,21 +53,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "world!",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // A list of bottom bar items
+                    MainScreen()
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
---
+++ app/src/main/java/com/cybersilicon/jetapp/screens/ProfileScreen.kt
@@ -0,0 +1,11 @@
+package com.cybersilicon.jetapp.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileScreen() {
+    Text(text = "Profile Screen")
+}
+
+
